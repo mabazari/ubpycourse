9bc9c8af2dcffc0104a3a724783e623b6a6e1239
mabazari <44176844+mabazari@users.noreply.github.com>
2018-10-16 22:17:04 +0330

    antonyms project

---
+++ antonyms.py
@@ -0,0 +1,22 @@
+antonym=dict()
+while True:
+    w1 = input("pleas enter a word=")
+    w1 = w1.lower()
+
+    if w1 in antonym.keys():
+        print(antonym[w1])
+
+    elif w1 in antonym.values():
+        for word,aword in antonym.items():
+            if aword==w1:
+                print(word)
+    else:
+        print("i dont know antonym of",w1)
+        b = input("do you know?(yes=y/no=n)")
+        b = b.lower()
+        if b == 'y':
+            w2 = input('what is it?')
+            w2 = w2.lower()
+            antonym[w1] = w2
+            q=u'Thanks \u2713'
+            print(q)
